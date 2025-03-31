3fcf8bcb67318d36a191e855c15e52bed5a1a1c8
Christoffer Høeg <clh@northarc.dk>
2025-04-01 01:13:16 +0200

    Config changes

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -30,7 +30,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'base.urls'
 
 TEMPLATES = [
     {
@@ -48,7 +48,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
